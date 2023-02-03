5973173e455deda7db272f0970e5ffe6301891e3
David Santacruz <david.santacruz@grupor5.com>
2023-02-03 15:01:26 -0500

    fist commit.

---
+++ examples/open_connection.rs
@@ -0,0 +1,76 @@
+use amqprs::{
+    callbacks::{DefaultChannelCallback, DefaultConnectionCallback},
+    channel::{BasicConsumeArguments, QueueBindArguments, QueueDeclareArguments},
+    connection::{Connection, OpenConnectionArguments},
+    consumer::DefaultConsumer,
+};
+use tokio::sync::Notify;
+
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 2)]
+async fn main() {
+    // construct a subscriber that prints formatted traces to stdout
+    // global subscriber with log level according to RUST_LOG
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .try_init()
+        .ok();
+
+    // open a connection to RabbitMQ server
+    let connection = Connection::open(&OpenConnectionArguments::new(
+        "localhost",
+        5672,
+        "user",
+        "bitnami",
+    ))
+        .await
+        .unwrap();
+    connection
+        .register_callback(DefaultConnectionCallback)
+        .await
+        .unwrap();
+
+    // open a channel on the connection
+    let channel = connection.open_channel(None).await.unwrap();
+    channel
+        .register_callback(DefaultChannelCallback)
+        .await
+        .unwrap();
+
+    // declare a queue
+    let (queue_name, _, _) = channel
+        .queue_declare(QueueDeclareArguments::default())
+        .await
+        .unwrap()
+        .unwrap();
+
+    // bind the queue to exchange
+    let rounting_key = "amqprs.example";
+    let exchange_name = "amq.topic";
+    channel
+        .queue_bind(QueueBindArguments::new(
+            &queue_name,
+            exchange_name,
+            rounting_key,
+        ))
+        .await
+        .unwrap();
+
+    //////////////////////////////////////////////////////////////////////////////
+    // start consumer, auto ack
+    let args = BasicConsumeArguments::new(&queue_name, "basic_consumer")
+        .no_ack(true)
+        .finish();
+
+    channel
+        .basic_consume(DefaultConsumer::new(args.no_ack), args)
+        .await
+        .unwrap();
+
+    // consume forever
+    println!("consume forever..., ctrl+c to exit");
+    let guard = Notify::new();
+    guard.notified().await;
+}
