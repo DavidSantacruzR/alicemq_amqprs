b5d25ddccbb63bcf47b30fb3a3eda389f10f54e6
David Santacruz <david.santacruz@grupor5.com>
2023-04-14 12:35:56 -0500

    feat: new branch for a full refactor.

--- examples/src/basic_consume_publish.rs
+++ examples/src/basic_consume_publish.rs
@@ -1,46 +1,7 @@
 use tokio;
-use tokio::sync::Notify;
-use tokio::time;
-use tracing::info;
-use alicemq::consumer::{Consumer};
-use alicemq::callback::{BaseCallbackConsumer};
-use alicemq::publisher::Publisher;
 
-async fn simple_callback(message: String) {
-    info!("received a message {:?}", message)
-}
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let new_event = "test_event".to_string();
-    let mut new_callback = BaseCallbackConsumer {no_ack: false, handler: Box::new((simple_callback))};
-    let mut consumer = Consumer::new()
-        .connect()
-        .await?
-        .set_queue_manager()
-        .build()
-        .unwrap()
-        .set_event_callback(new_event, new_callback);
+async fn main() {
 
-    consumer
-        .start_consumer()
-        .await?;
-
-    let data = "{data: value}";
-
-    time::sleep(time::Duration::from_secs(5)).await;
-
-    let publisher = Publisher::new()
-        .connect()
-        .await.unwrap()
-        .build()
-        .unwrap();
-
-    for _ in 1 .. 10 {
-        publisher.clone().send_message("test_event".to_string(), data.to_string()).await;
-    }
-    publisher.close().await;
-    let guard = Notify::new();
-    guard.notified().await;
-    Ok(())
 }
--- examples/src/basic_consume_publish_no_ack.rs
+++ examples/src/basic_consume_publish_no_ack.rs
@@ -1,44 +1,7 @@
 use tokio;
-use tokio::sync::Notify;
-use tokio::time;
-use alicemq::consumer::{Consumer};
-use alicemq::callback::{BaseCallbackConsumer};
-use alicemq::publisher::Publisher;
+
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let new_event = "test_event_manual_ack".to_string();
+async fn main() {
 
-    //In the callback set whether you want auto acknowledgement of messages.
-    let new_callback = BaseCallbackConsumer::new(true);
-    let mut consumer = Consumer::new()
-        .connect()
-        .await?
-        .set_queue_manager()
-        .build()
-        .unwrap()
-        .set_event_callback(new_event, new_callback);
-
-    /*Starts the consumer, and keep it running, if out scope closes active connections.*/
-    consumer
-        .start_consumer()
-        .await?;
-
-    let data = "{data: {id: 1, value: None}";
-
-    time::sleep(time::Duration::from_secs(5)).await;
-
-    let publisher = Publisher::new()
-        .connect()
-        .await.unwrap()
-        .build()
-        .unwrap();
-
-    for _ in 1 .. 10 {
-        publisher.clone().send_message("test_event_manual_ack".to_string(), data.to_string()).await;
-    }
-    publisher.close().await;
-    let guard = Notify::new();
-    guard.notified().await;
-    Ok(())
 }
