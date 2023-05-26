5a15396d296be22cfd3e5db66b0f652602671b56
David Santacruz R <axelsantacruzr@gmail.com>
2023-05-26 18:32:08 +0100

    fix: added send message complete method to actually publish to the stream.

--- alicemq/src/publisher.rs
+++ alicemq/src/publisher.rs
@@ -1,7 +1,10 @@
-use amqprs::{connection::Connection, channel::Channel};
+use amqprs::{connection::Connection, channel::Channel, BasicProperties};
+use amqprs::channel::{BasicPublishArguments, QueueBindArguments};
 use amqprs::connection::OpenConnectionArguments;
 use crate::callbacks::{CustomChannelCallback, CustomConnectionCallback};
 use crate::settings::base::{Config};
+use tokio::time;
+use tracing::info;
 
 #[derive(Clone)]
 pub struct Publisher {
@@ -26,7 +29,7 @@ impl Publisher {
         self.connection = connection;
         self
     }
-    pub async fn send_message(mut self, data: String) {
+    pub async fn send_message(mut self, data: String, queue: String) {
         let new_channel = self.connection.as_ref().expect("Unable to get connection")
             .open_channel(None)
             .await
@@ -36,9 +39,26 @@ impl Publisher {
             .await
             .unwrap();
 
-        new_channel.send
+        let delivered_content = data.clone().into_bytes();
+        let publishing_args = BasicPublishArguments::new("amq.topic", "amqprs.example");
+        let channel = self.connection.open_channel(None).await.unwrap();
+        new_channel
+            .queue_bind(QueueBindArguments::new(
+                &queue,
+                "amp.topic",
+                "amqprs.example"
+            ))
+            .await
+            .unwrap();
+        channel
+            .register_callback(CustomConnectionCallback)
+            .await
+            .unwrap();
+        channel
+            .basic_publish(BasicProperties::default(), delivered_content, publishing_args)
+            .await
+            .unwrap();
+        time::sleep(time::Duration::from_millis(100)).await;
+        info!("message sent with data: {:?}", data);
     }
 }
-
-
-
