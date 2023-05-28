0b1388e28cb388d2f3ce35d02030e95f886a155d
David Santacruz R <axelsantacruzr@gmail.com>
2023-05-28 04:12:26 +0100

    new methods

--- examples/src/handler_consumer.rs
+++ examples/src/handler_consumer.rs
@@ -19,7 +19,6 @@ impl AsyncConsumer for ConsumerCallback {
         _basic_properties: BasicProperties,
         _content: Vec<u8>,
     ) {
-        info!("test consumer delivery {} on channel {}", deliver, channel);
         info!("got message with data {}", std::str::from_utf8(&_content).unwrap());
         if !self.no_ack {
             let args = BasicAckArguments::new(deliver.delivery_tag(), false);
@@ -37,22 +36,18 @@ async fn main() {
         .await
         .build();
 
-    test_consumer
+    let data_to_send: String = "data: {'field': 'name'}".to_string();
+    let queue: String = "test_event".to_string();
+    let message_publisher = Publisher::new();
+
+    /*test_consumer
         .set_event_queue(
             "test_event".to_string(),
             ConsumerCallback {no_ack: false}
         ).await
-        .run(true).await;
+        .run(true).await;*/
 
-    let data_to_send: String = "data: {'field': 'name'}".to_string();
-    let queue: String = "test_event".to_string();
-    let message_publisher = Publisher::default();
-
-    for _ in  1 .. 10 {
-        message_publisher.clone()
-            .connect()
-            .await
-            .send_message(data_to_send.clone(), queue.clone())
-            .await
-    }
+    message_publisher
+        .send_message(data_to_send.clone(), queue.clone())
+        .await;
 }
