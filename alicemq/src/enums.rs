c020740f9919611700bd197e90c38a0ae2665d34
David Santacruz R <axelsantacruzr@gmail.com>
2024-01-31 22:55:50 -0500

    feat: started re writte.

--- alicemq/src/enums.rs
+++
@@ -1,6 +0,0 @@
-
-#[derive(PartialEq, Clone)]
-pub enum Runtime {
-    ASYNCHRONOUS,
-    SYNCHRONOUS
-}
--- alicemq/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod consumer;
-pub mod settings;
-pub mod publisher;
-mod callbacks;
-pub mod base;
-pub mod enums;
-pub mod traits;
