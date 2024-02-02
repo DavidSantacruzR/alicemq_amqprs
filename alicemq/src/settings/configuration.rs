a1516fcdbb1028d089a172d0998adccd80c212d1
David Santacruz <david.santacruz@grupor5.com>
2024-02-02 12:04:21 -0500

    feat: completed test for loading settings, and refactored code.

--- alicemq/src/settings/configuration.rs
+++ alicemq/src/settings/configuration.rs
@@ -1,6 +1,8 @@
 use dotenv::dotenv;
 use std::collections::HashMap;
+use crate::settings::constants::{DEFAULT_HOST, DEFAULT_PASSWORD, DEFAULT_PORT, DEFAULT_USER};
 
+#[derive(PartialEq, Debug)]
 struct ConnectionSettings {
     host: String,
     port: u16,
@@ -9,26 +11,59 @@ struct ConnectionSettings {
 }
 
 impl ConnectionSettings {
-    fn new(host:String, port: u16, username: String, password: String) -> ConnectionSettings {
+
+    #[allow(dead_code)]
+    fn new() -> ConnectionSettings {
+        let _settings = ConnectionSettings::load_settings();
         ConnectionSettings {
-           host, port, username, password
+            host: String::from(_settings.get("HOST").unwrap()),
+            port: _settings.get("PORT").unwrap().parse().expect("Unable to parse port number."),
+            username: String::from(_settings.get("USERNAME").unwrap()),
+            password: String::from(_settings.get("PASSWORD").unwrap())
         }
     }
 
+    #[allow(dead_code)]
     fn load_settings() -> HashMap<&'static str, String> {
         dotenv().ok();
         let mut connection_settings = HashMap::new();
-        for key in ["HOST", "PORT", "USERNAME", "PASSWORD"] {
-            connection_settings.insert(key, std::env::var("HOST").unwrap());
-        };
+        connection_settings.insert("HOST", match std::env::var("HOST") {
+            Ok(value) => { value }
+            Err(_) => { DEFAULT_HOST.to_string() }
+        });
+        connection_settings.insert("PORT", match std::env::var("PORT") {
+            Ok(value) => { value },
+            Err(_) => { DEFAULT_PORT.to_string() }
+        });
+        connection_settings.insert("USERNAME", match std::env::var("USERNAME") {
+            Ok(value) => { value },
+            Err(_) => { DEFAULT_USER.to_string() }
+        });
+        connection_settings.insert("PASSWORD", match std::env::var("PASSWORD") {
+            Ok(value) => { value },
+            Err(_) => { DEFAULT_PASSWORD.to_string() }
+        });
         connection_settings
     }
 }
 
 #[cfg(test)]
 mod tests {
-    fn test_load_env_settings() {}
+    use crate::settings::configuration::ConnectionSettings;
 
+    #[test]
+    fn test_load_env_settings() {
+        let _settings = ConnectionSettings::new();
+        let _manual_settings_object = ConnectionSettings {
+            host: "localhost".to_string(),
+            port: 5672,
+            username: "guest".to_string(),
+            password: "guest".to_string(),
+        };
+        assert_eq!(_settings, _manual_settings_object)
+    }
+
+    #[test]
     fn test_load_settings_default() {}
 }
 
