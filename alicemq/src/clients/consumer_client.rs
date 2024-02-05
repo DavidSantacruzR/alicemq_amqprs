ea2245eb6f4b3ed004303cbcbe663298bc006e25
David Santacruz R <axelsantacruzr@gmail.com>
2024-02-04 19:41:44 -0500

    fix: changed logger for debug.

--- alicemq/src/clients/consumer_client.rs
+++ alicemq/src/clients/consumer_client.rs
@@ -1,7 +1,7 @@
 use amqprs::channel::Channel;
 use amqprs::connection::{Connection, OpenConnectionArguments};
 use tokio::sync::Notify;
-use tracing::{info};
+use tracing::{debug};
 use crate::settings::configuration::ConnectionSettings;
 
 #[allow(dead_code)]
@@ -18,7 +18,7 @@ impl ConsumerManager {
 
     pub async fn run(self, long_lived: bool) {
         if long_lived {
-            info!("Consuming messages from queue.");
+            debug!("Consuming messages from queue.");
             let _guard = Notify::new();
             _guard.notified().await;
         } else {
